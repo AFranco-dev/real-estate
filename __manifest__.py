a015a66021ca26e2884a3fd0f8eba675d46ba4f5
Andre Franco <andre.franco.dev@gmail.com>
2022-12-08 08:32:37 -0600

    added security file to manifest

--- __manifest__.py
+++ __manifest__.py
@@ -11,7 +11,7 @@
         'mail',
     ],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
     ],
