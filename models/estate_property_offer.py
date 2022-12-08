06b52e6d54fc5d50d745f2d0f8c3009207bda712
Andre Franco <andre.franco.dev@gmail.com>
2022-12-08 15:41:37 -0600

    fixed views widget and nnames

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -6,10 +6,10 @@ class EstatePropertyOffer(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Property Offer'
 
-    price = fields.Float(string="Monto", required=True)
+    price = fields.Float(string="Amount", required=True)
     status = fields.Selection(
         string='Offer Status', 
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
         help='This is the approval status of the order.')
-    partner_id = fields.Many2one('res.partner', required=True)
-    property_id = fields.Many2one('estate.property', required=True)
+    partner_id = fields.Many2one('res.partner', required=True, string="Offerer")
+    property_id = fields.Many2one('estate.property', required=True, string="Property")
