960ca10e231feff65af65cd1085d64423bd932d6
Andre Franco <andre.franco.dev@gmail.com>
2022-12-09 11:25:02 -0600

    added computed values of fixed deadline and validuty

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
+from odoo.tools import date_utils
+
 
 class EstatePropertyOffer(models.Model):
     _name = 'estate.property.offer'
@@ -12,4 +14,17 @@ class EstatePropertyOffer(models.Model):
         selection=[('accepted', 'Accepted'), ('refused', 'Refused')],
         help='This is the approval status of the order.')
     partner_id = fields.Many2one('res.partner', required=True, string="Offerer")
+    validity = fields.Integer(string="Validity Days", default=7)
+
+    date_deadline = fields.Date(string="Offer Deadline", compute="_compute_deadline", inverse="_inverse_deadline")
+
     property_id = fields.Many2one('estate.property', required=True)
+
+    @api.depends('validity', 'date_deadline')
+    def _compute_deadline(self):
+        for rec in self:
+            rec.date_deadline = date_utils.add(fields.Date.today(), days = rec.validity)
+
+    def _inverse_deadline(self):
+        for rec in self:
+            rec.validity = date_utils.subtract(rec.date_deadline, fields.Date.today()).days
