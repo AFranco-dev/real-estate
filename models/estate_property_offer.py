84bfc7fbb962ce551cc3ff9f7ec49c7b24768e21
Andre Franco <andre.franco.dev@gmail.com>
2022-12-09 11:48:22 -0600

    fixed compute function

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -21,7 +21,7 @@ class EstatePropertyOffer(models.Model):
 
     property_id = fields.Many2one('estate.property', required=True)
 
-    @api.depends('validity', 'date_deadline')
+    @api.depends('validity')
     def _compute_deadline(self):
         for rec in self:
             rec.date_deadline = date_utils.add(date.today(), days = rec.validity)
