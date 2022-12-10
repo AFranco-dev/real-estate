49184f93f417dfe8276443175e95ab5be48f820a
Andre Franco <andre.franco.dev@gmail.com>
2022-12-10 09:49:23 -0600

    removed readonly attribute

--- models/estate_property.py
+++ models/estate_property.py
@@ -15,7 +15,7 @@ class EstateProperty(models.Model):
         string='Date Availability', 
         copy=False, 
         default=lambda self: date_utils.add(fields.Date.today(), months=3))
-    expected_price = fields.Float(string='Expected Price', readonly=True)
+    expected_price = fields.Float(string='Expected Price')
     selling_price = fields.Float(string='Selling Price', required=True, copy=False)
     bedrooms = fields.Integer(string='# Of Bedrooms', default=3)
     living_area = fields.Integer(string='# Of Living Areas')
