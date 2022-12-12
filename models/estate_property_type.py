23f46a2481cf78d83c0d1e8bfffac58ce5ad7293
Andre Franco <andre.franco.dev@gmail.com>
2022-12-12 16:54:31 -0600

    fixed button

--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -13,9 +13,12 @@ class EstatePropertyType(models.Model):
     estate_property_ids = fields.One2many('estate.property', 'estate_property_type_id')
     estate_property_offer_ids = fields.One2many('estate.property.offer', 'property_type_id')
 
-    offers_quantity = fields.Float(string="Best Offer", compute= lambda self: len(self.estate_property_offer_ids))
+    offers_quantity = fields.Integer(string="Best Offer", compute='_offers_quantity')
 
     _sql_constraints = [
         ('check_name_is_unique', 'unique(name)',
         'There is a property tag with that name already!')
     ]
+
+    def _offers_quantity(self):
+        return len(self.estate_property_offer_ids)
