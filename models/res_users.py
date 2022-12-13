408f22656cd0cb8c057926cc4835500b69868c8e
Andre Franco <andre.franco.dev@gmail.com>
2022-12-13 15:32:22 -0600

    added res_users inherited model

--- models/res_users.py
+++ models/res_users.py
@@ -4,5 +4,5 @@ class Users(models.Model):
     _inherit = 'res.users'
 
     property_ids = fields.One2many('estate.property', 'salesperson_id', 
-        domain = [('date_availability', '&lt;=', fields.Date.today())],
+        domain = [('date_availability', '<=', fields.Date.today())],
         string="Seller Available Properties")
