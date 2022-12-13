567b91f493d7541d68d4eccfdd8ac1774b4b4498
Andre Franco <andre.franco.dev@gmail.com>
2022-12-13 14:49:00 -0600

    added res_users inherited model

--- models/res_users.py
+++ models/res_users.py
@@ -1,6 +1,6 @@
 from odoo import api, fields, models, exceptions
 
-class EstateProperty(models.Model):
+class ResUsers(models.Model):
     _inherit = ['res.users']
 
     property_ids = fields.One2many('estate.property', 'salesperson_id', 
